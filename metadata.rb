7bd844227f836b22bf21f3ed147340d090523060
Roy Ratcliffe <roy@pioneeringsoftware.co.uk>
2014-12-28 14:56:22 +0000

    Depends on apt and ssl_certificate cookbooks

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,9 @@ description      'Installs/Configures ssl_lb'
 long_description 'Installs/Configures ssl_lb'
 version          '0.1.0'
 
-%w(haproxy).each do |cb|
+# Depends on `apt` and `ssl_certificate` cookbooks for SSL. Apt needs updating
+# for the latest version of OpenSSL development package. The `ssl_certificate`
+# cookbook has providers for self-signed certificate generation.
+%w(haproxy apt ssl_certificate).each do |cb|
   depends cb
 end
